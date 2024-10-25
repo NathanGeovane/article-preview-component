6f7656b88e5c9fc1e652c664ca1d18550ce1e20d
Nathan Geovane <nathan.geovane1@gmail.com>
2024-10-25 17:37:42 -0300

    refactoring the btns logic inside a function

--- index.js
+++ index.js
@@ -5,19 +5,27 @@ const shareBtnExit = document.querySelector(".btn-share--exit");
 const shareOverlay = document.querySelector(".btn-share--overlay");
 const shareBtns = [shareBtn, shareBtnExit];
 
+function btnsLogic() {
+  if (shareOverlay.classList.contains("active") && window.innerWidth < 870) {
+    shareBtn.classList.add("hidden");
+  } else {
+    shareBtn.classList.remove("hidden");
+    shareBtn.classList.add("active");
+  }
+  if (window.innerWidth > 870 && shareOverlay.classList.contains("active")) {
+    shareBtn.classList.remove("hidden");
+  } else {
+    shareBtn.classList.remove("active");
+  }
+}
+
 shareBtns.map((btn) =>
   btn.addEventListener("click", () => {
     shareOverlay.classList.toggle("active");
-    if (shareOverlay.classList.contains("active") && window.innerWidth < 870) {
-      shareBtn.classList.add("hidden");
-    } else {
-      shareBtn.classList.remove("hidden");
-    }
-    if (window.innerWidth > 870 && shareOverlay.classList.contains("active")) {
-      shareBtn.classList.add("active");
-      shareBtn.classList.remove("hidden");
-    } else {
-      shareBtn.classList.remove("active");
-    }
+    btnsLogic();
   })
 );
+
+window.addEventListener("resize", function () {
+  btnsLogic();
+});
